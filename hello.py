482bb3fa97d0f2ab9d65ab82b962b3fc7eca52eb
Mehar <meharsaikuna2022@gmail.com>
2024-11-23 11:01:49 +0000

    hello.py added

--- hello.py
+++ hello.py
@@ -1,2 +1,6 @@
 print ("this is created in dev branch")
-print ("this is second line in dev")       
+print ("this is second line in dev")  
+
+Print ("this is 3rd line")
+
+print("to check cherry pic")
